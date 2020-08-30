bcf5cfb22bc74159a87c245a6d63de36d6e33326
Sajawal Javaid <sajawaljavaid@hotmail.com>
2020-08-30 17:26:18 -0400

    UI Updates - Added basic login and dashboard pages - Added additional material designs - Added two routes (/login, /dashboard)

---
+++ public/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { DashboardComponent } from './core/dashboards/dashboard/dashboard.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginPageComponent } from './core/auth/login-page/login-page.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent, data: { pageType: 'dashboard' } },
+  { path: 'login', component: LoginPageComponent, data: { pageType: 'login' } },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { LoginPageComponent } from './core/auth/login-page/login-page.component';
+import { DashboardComponent } from './core/dashboards/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
